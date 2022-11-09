a16e2ee180eb4ec67cfa61c623c4a410ab407d47
Anik Ronjon <anik35-497@diu.edu.bd>
2022-11-09 21:57:04 +0600

    PostgreSQL database added

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -63,8 +63,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'travel',
+        'USER': 'postgres',
+        'PASSWORD': 'engineer',
+        'HOST': 'localhost',
+        'PORT': ''
     }
 }
 
