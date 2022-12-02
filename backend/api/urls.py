761d29da27f83d8dc08729014cf443a21b4cc1d8
Anik Ronjon <anik35-497@diu.edu.bd>
2022-12-02 17:13:56 +0600

    serializer and views created

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,17 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('division', views.DivisionModelView, basename='division')
+router.register('district', views.DistrictModelView)
+router.register('location', views.LocationModelView)
+router.register('restaurant', views.RestaurantModelView)
+router.register('hotel', views.HotelModelView)
+router.register('agency', views.AgencyModelView)
 
 
 app_name = 'api'
 urlpatterns = [
-    path('', views.home_view, name='home-page'),
-]
+    path('auth/', include('rest_framework.urls'))
+] + router.urls
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,34 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework.viewsets import ReadOnlyModelViewSet
+from .models import Division, District, Location, Restaurant, Hotel, Agency
+from . import serializers
 
 
-# Create your views here.
-def home_view(request):
-    return HttpResponse("<h2>Home page</h2>")
+class DivisionModelView(ReadOnlyModelViewSet):
+    queryset = Division.objects.all()
+    serializer_class = serializers.DivisionSerializer
+
+
+class DistrictModelView(ReadOnlyModelViewSet):
+    queryset = District.objects.all()
+    serializer_class = serializers.DistrictSerializer
+
+
+class LocationModelView(ReadOnlyModelViewSet):
+    queryset = Location.objects.all()
+    serializer_class = serializers.LocationSerializer
+
+
+class RestaurantModelView(ReadOnlyModelViewSet):
+    queryset = Restaurant.objects.all()
+    serializer_class = serializers.RestaurantSerializer
+
+
+class HotelModelView(RestaurantModelView):
+    queryset = Hotel.objects.all()
+    serializer_class = serializers.HotelSerializer
+
+
+class AgencyModelView(ReadOnlyModelViewSet):
+    queryset = Agency.objects.all()
+    serializer_class = serializers.AgencySerializer
+
